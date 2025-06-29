ae3340b321995d5b509e8f2ce8069ff11471ba48
Síria Cabral <123579270+siriacfreitas@users.noreply.github.com>
2025-06-29 16:21:25 -0300

    I2C STM ESCRAVO

---
+++ I2C/STM32_Escravo/master_sender/master_sender.ino
@@ -0,0 +1,39 @@
+// ===== CÓDIGO PARA O ESP32 MESTRE (MASTER) =====
+// Ele envia os comandos '1' e '0' para controlar o LED do escravo.
+
+#include <Wire.h>
+
+// Endereço I2C do escravo que queremos controlar. Deve ser o mesmo do código do escravo.
+const byte ENDERECO_ESCRAVO = 0x2A;
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Inicia a comunicação I2C como Mestre.
+  Wire.begin(); 
+  
+  Serial.println("ESP32 Mestre pronto para enviar comandos.");
+}
+
+void loop() {
+  // --- Envia comando para LIGAR o LED ---
+  Wire.beginTransmission(ENDERECO_ESCRAVO); // Inicia a transmissão para o escravo
+  Wire.write('1');                           // Envia o byte/caractere '1'
+  Wire.endTransmission();                    // Finaliza e envia os dados
+  
+  // Imprime no serial do Mestre o que ele fez.
+  Serial.println("Comando '1' (LIGAR) enviado.");
+  
+  delay(2000); // Espera 2 segundos
+  
+  
+  // --- Envia comando para DESLIGAR o LED ---
+  Wire.beginTransmission(ENDERECO_ESCRAVO); // Inicia a transmissão para o escravo
+  Wire.write('0');                           // Envia o byte/caractere '0'
+  Wire.endTransmission();                    // Finaliza e envia os dados
+
+  // Imprime no serial do Mestre o que ele fez.
+  Serial.println("Comando '0' (DESLIGAR) enviado.");
+
+  delay(2000); // Espera 2 segundos
+}
